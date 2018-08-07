49ff4b527c0a463b09bf8b78ef98a11e71e1b521
Mason Stallmo <masonstallmo@gmail.com>
2018-08-06 20:05:02 -0500

    Composition with Person and Sayian

--- main.go
+++ main.go
@@ -2,24 +2,23 @@ package main
 
 import "fmt"
 
-type Saiyan struct {
-	Name  string
+type Person struct {
+	Name string
+}
+
+func (p *Person) Introduce() {
+	fmt.Printf("Hi, I'm %s\n", p.Name)
+}
+
+type Sayian struct {
+	*Person
 	Power int
 }
 
-func NewSaiyan(name string, power int) *Saiyan {
-	return &Saiyan{
-		Name:  name,
-		Power: power,
-	}
-}
-
-func (s *Saiyan) Super() {
-	s.Power += 10000
-}
-
 func main() {
-	goku := NewSaiyan("goku", 9000)
-	goku.Super()
-	fmt.Println(goku.Power)
+	goku := &Sayian{
+		Person: &Person{"Goku"},
+		Power:  9001,
+	}
+	goku.Introduce()
 }
